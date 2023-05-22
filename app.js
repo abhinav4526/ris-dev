b496051cf897008c565958b7e272bd93424fce8a
abhinav4526 <abhinavkumar4526@gmail.com>
2023-05-22 19:00:31 +0530

    add: nested tags

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-const heading = React.createElement("h1", {}, "Hello rrrrrrrr !");
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement(
+    "div",
+    { id: "child1" },
+    React.createElement("h1", {}, "Hello RRRRRRR !"),
+    React.createElement("h2", {}, "Hello rrrrrrr !")
+  ),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "Hello RRRRRRRR !"),
+    React.createElement("h2", {}, "Hello rrrrrrrr !"),
+  ]),
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
