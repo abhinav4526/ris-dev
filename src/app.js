b1047f6372f35a59f39611e86586481dff4fa192
abhinav4526 <abhinavkumar4526@gmail.com>
2023-05-30 09:09:18 +0530

    add: about page, page not found, client side routing

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import Header from "./components/header";
 import Body from "./components/body";
+import About from "./components/about";
+import Error from "./components/error";
 
 const AppLayout = () => {
   return (
@@ -11,5 +14,17 @@ const AppLayout = () => {
   );
 };
 
+const appRoute = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRoute} />);
---
+++ src/components/about.js
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Hello this is page is coming soon....</p>
+    </div>
+  );
+};
+
+export default About;
